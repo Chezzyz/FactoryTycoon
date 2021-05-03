20dbb7652d5d5308b3421df6ac20258606986588
TerribleVoice <terriblevoice@ya.ru>
2021-05-03 18:55:35 +0500

    Calculation Scripts

---
+++ FactoryTycoon/Assets/Scripts/Divistion.cs
@@ -0,0 +1,20 @@
+﻿public class Division : IOperation
+{
+    private readonly float leftOperand;
+    private readonly float rightOperand;
+    private float result;
+
+    public Division(float left, float right)
+    {
+        leftOperand = left;
+        rightOperand = right;
+
+        result = left / right;
+    }
+
+    public Division(IOperation left, float right) : this(left.Result, right)
+    {
+    }
+
+    public float Result => result;
+}
---
+++ FactoryTycoon/Assets/Scripts/Subtraction.cs
@@ -0,0 +1,20 @@
+﻿public class Subtraction : IOperation
+{
+    private readonly float leftOperand;
+    private readonly float rightOperand;
+    private float result;
+
+    public Subtraction(float left, float right)
+    {
+        leftOperand = left;
+        rightOperand = right;
+
+        result = left - right;
+    }
+
+    public Subtraction(IOperation left, float right) : this(left.Result, right)
+    {
+    }
+
+    public float Result => result;
+}
