fd4d090126b89bb5e121891f0f1e22e0c904bb48
Denis <47297652+Da1W@users.noreply.github.com>
2021-05-07 21:43:45 +0500

    CellController

---
+++ FactoryTycoon/Assets/Scripts/CellController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CellController : Button
+{
+    //private Button button;
+    //private Action<string> link = Link => {
+    //    string.Join("", GetComponentInParent<Table>().gameObject.name, GetComponentInParent<VerticalLayoutGroup>().gameObject.name, );
+    //}
+
+    public override void OnPointerEnter(PointerEventData eventData)
+    {
+
+        if (MouseProperties.isLeftButtonDown)
+            base.OnSelect(eventData);
+
+
+        //base.OnPointerEnter(eventData);
+    }
+    public override void OnPointerExit(PointerEventData eventData)
+    {
+        //base.OnPointerExit(eventData);
+    }
+
+    public override void OnMove(AxisEventData eventData)
+    {
+        //base.OnMove(eventData);
+    }
+
+    public override void OnSelect(BaseEventData eventData)
+    {
+        base.OnSelect(eventData);
+    }
+
+
+}
---
+++ FactoryTycoon/Assets/Scripts/MouseProperties.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MouseProperties
+{
+    public static bool isLeftButtonDown;
+    
+}
