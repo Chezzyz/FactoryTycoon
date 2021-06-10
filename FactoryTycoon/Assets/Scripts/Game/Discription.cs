3c7fbc25e2cf20bc29f583a398e0069b630180a9
Denis <47297652+Da1W@users.noreply.github.com>
2021-06-10 21:51:39 +0500

    Discription and tips

---
+++ FactoryTycoon/Assets/Scripts/Game/Discription.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Discription : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] GameObject discriptionWindow;
+
+    private bool isOnCard = false;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        discriptionWindow.SetActive(true);
+        isOnCard = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        discriptionWindow.SetActive(false);
+        isOnCard = false;
+    }
+    void Update()
+    {
+        if (isOnCard)
+        {
+            discriptionWindow.transform.position = Input.mousePosition;
+        }
+    }
+}
---
+++ FactoryTycoon/Assets/Scripts/Game/TipMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TipMovement : MonoBehaviour
+{
+    [SerializeField] GameObject tipImageGO;
+    [SerializeField] GameObject startPoint;
+    [SerializeField] GameObject endPoint;
+    [SerializeField] private float forwardSpeed = 3;
+    [SerializeField] private float returnSpeed = 1;
+    [SerializeField] private float holdingTime = 0.5f;
+    [SerializeField] bool isLoop = true;
+    private Tweener _move;
+    public void StartTipAnimation()
+    {
+        tipImageGO.SetActive(true);
+        tipImageGO.transform.position = startPoint.transform.position;
+
+        StartCoroutine(MoveToPoint());
+    }
+
+    private IEnumerator MoveToPoint()
+    {
+        _move = tipImageGO.transform.DOMove(endPoint.transform.position, forwardSpeed);
+
+        yield return _move.WaitForCompletion();
+        yield return new WaitForSeconds(holdingTime);
+
+        _move = tipImageGO.transform.DOMove(startPoint.transform.position, returnSpeed);
+
+        yield return _move.WaitForCompletion();
+
+
+        if (isLoop)
+        {
+            StartCoroutine(MoveToPoint());
+        }
+    }
+
+    public void StopAnimation()
+    {
+        _move.Pause();
+        tipImageGO.SetActive(false);
+    }
+}
